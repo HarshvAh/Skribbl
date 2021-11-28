bc2b1cf3a20bdd45724381e80a997b1214e459ad
PDS16 <81670503+PDS16@users.noreply.github.com>
2021-11-29 00:04:33 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+mkdir server
+mkdir client
+cd server
+npm init
+npm i express nodemon socket.io cors
+cd ..
+cd client
+npx create-react-app .
+npm i socket.io-client
+npm i react-scroll-to-bottom
+cd ..
+
+
